f30134cad95c980b350f5223c6ee9c8e76c0b546
dkarzanov <dkarzanov@samsmu.local>
2024-03-10 13:54:38 +0400

    PlusOne_66

--- PlusOne_66/main.go
+++ PlusOne_66/main.go
@@ -1,1 +1,30 @@
 package main
+
+import "fmt"
+
+func recursiveCheck(digits []int, index int) []int {
+
+	if digits[index] != 9 {
+		digits[index] += 1
+		return digits
+	}
+
+	if index == 0 && digits[0] == 9 {
+		digits[0] = 0
+		digits = append([]int{1}, digits...)
+		return digits
+	}
+
+	digits[index] = 0
+	return recursiveCheck(digits, index-1)
+}
+
+func plusOne(digits []int) []int {
+	n := len(digits)
+
+	return recursiveCheck(digits, n-1)
+}
+
+func main() {
+	fmt.Print(plusOne([]int{1, 9, 9, 0}))
+}
