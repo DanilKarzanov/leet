a4a5cc93c152936d49c883d947112cc276a54483
Daniil Karzanov <d.carzanov@gmail.com>
2025-01-30 21:08:26 +0400

    Sleep_2621

---
+++ Sleep_2621/index.ts
@@ -0,0 +1,12 @@
+async function sleep(millis: number): Promise<void> {
+    return new Promise((res, _rej) => {
+        setTimeout(() => {
+            res()
+        }, millis)   
+    })    
+}
+
+/** 
+ * let t = Date.now()
+ * sleep(100).then(() => console.log(Date.now() - t)) // 100
+ */
